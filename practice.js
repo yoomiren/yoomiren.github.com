01e8e5832f56a34abc82637695476b3e87484f8b
yoomiren <renz796@newschool.edu>
2018-04-11 11:30:20 -0400

    04/11

---
+++ practice.js
@@ -0,0 +1,13 @@
+function funct(){
+var x = "CODING IS TOO HARD";
+document.getElementById("text").innerHTML= x + ", CODING IS TOO HARD";
+}
+function hide(){
+document.getElementById("text").style.visibility="hidden";
+}
+function show(){
+document.getElementById("text").style.visibility="visible";
+}
+function reset(){
+document.getElementById("text").innerHTML= "hello again" ;
+}
