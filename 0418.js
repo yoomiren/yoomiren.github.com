a3b136ceff5e0aceac705540cf45a2257db61e8a
yoomiren <renz796@newschool.edu>
2018-04-20 09:58:58 -0400

    04/20

---
+++ 0418.js
@@ -0,0 +1,16 @@
+var item = [" "];
+
+$ (#submit").on("click"), function () {
+var list-item = {
+	first_name:$("#first_name").val ()
+}
+
+item.push (list_item);
+var output = $("#output");
+var temp = "";
+for (var i =0; i < items.length; i++) {
+	temp = temp + "<p>" + items [i] + "</p>"
+}
+
+output.html (temp);
+});2
