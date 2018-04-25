cc9b393c8e0205b9a8241a5bc1252dd29a22ed71
yoomiren <renz796@newschool.edu>
2018-04-25 12:29:46 -0400

    0425

---
+++ labfinal.js
@@ -0,0 +1,50 @@
+$('#quiz').quiz({
+  
+
+  counterFormat: 'Question %current of %total',
+ 
+  questions: [
+    {
+      'q': 'Is coding hard?',
+      'options': [
+        'Yes',
+        'No'
+      ],
+      'correctIndex': 0,
+      'correctResponse': 'of course',
+      'incorrectResponse': 'Sorry, wrong answer.'
+    },
+    {
+      'q': 'question 2',
+      'options': [
+        'answer 1',
+        'answer 2',
+        'answer 3'
+      ],
+      'correctIndex': 2,
+      'correctResponse': 'oops',
+      'incorrectResponse': 'Great job!'
+    },
+    {
+      'q': 'question 3',
+      'options': [
+        'True',
+        'False'
+      ],
+      'correctIndex': 0,
+      'correctResponse': 'Great job!',
+      'incorrectResponse': 'wrong'
+    },
+    {
+      'q': 'question 4',
+      'options': [
+        'answer 1',
+        'answer 2',
+        'answer 3'
+      ],
+      'correctIndex': 2,
+      'correctResponse': 'Correct! ',
+      'incorrectResponse': 'Wrong!'
+    }
+  ]
+});
